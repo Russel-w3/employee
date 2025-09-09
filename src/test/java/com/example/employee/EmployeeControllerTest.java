e73156b3bf6188452e37df792add4236dc647fab
WANGRU3 <RUSSEL.WANG@OOCL.COM>
2025-09-09 18:52:45 +0800

    refactor: remove unused import

--- src/test/java/com/example/employee/EmployeeControllerTest.java
+++ src/test/java/com/example/employee/EmployeeControllerTest.java
@@ -8,10 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
